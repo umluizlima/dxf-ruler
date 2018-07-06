956ce7a813c63629321516ff71e281e733033625
umluizlima <umluizlima@gmail.com>
2018-07-06 12:44:35 -0300

    Get abspath from basename.

--- app/controller/api.py
+++ app/controller/api.py
@@ -1,3 +1,4 @@
+import os.path
 from subprocess import check_output
 from flask import (
     Blueprint, send_file
@@ -9,10 +10,10 @@ bp = Blueprint('api', __name__)
 @bp.route('/<int:length>')
 def ruler(length):
     """Generate and send ruler of given length."""
-    filepath = check_output(
+    filename = os.path.basename(check_output(
         f'dxf-ruler-generator {length}'
-    ).decode('utf-8')
+    ).decode('utf-8'))
     return send_file(
-        filepath,
+        os.path.abspath(filename),
         as_attachment=True
     )
